35df98c5f0706ccd86b6578f58b18bfd4d7f7350
Jorge Canche <jorge.canche@adira.com.mx>
2020-12-19 14:13:51 -0600

    Fix model medico y paciente: imports y warnings

--- src/main/java/com/example/demo/model/Medico.java
+++ src/main/java/com/example/demo/model/Medico.java
@@ -1,11 +1,8 @@
-package java.com.example.demo.model;
+package com.example.demo.model;
 
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,9 +10,6 @@ import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "doctor")
