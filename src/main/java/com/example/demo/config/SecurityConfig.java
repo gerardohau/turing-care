df9eaed1d64352fefe777bd516fde99411f41e14
Jorge Canche <jcanchecauich@gmail.com>
2021-01-04 14:06:05 -0600

    seguridad inicial falta repo usuario

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import mx.ourpodcast.repository.UsuarioRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        TokenFilter tokenFiltro = new TokenFilter(usuarioRepository);
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .csrf().disable() 
+            .httpBasic().disable()
+            .authorizeRequests()
+            .antMatchers("/login", "/registrar","/").permitAll()
+            .anyRequest().authenticated()
+            .and().logout().logoutUrl("/logout")
+            //.anyRequest().permitAll()
+            .and()
+            .addFilterBefore(tokenFiltro, BasicAuthenticationFilter.class);
+    }
+}
