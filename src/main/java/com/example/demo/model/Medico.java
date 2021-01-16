92f8ca249193170ab9a17e62b1dba7165fb297a9
Jorge Canche <jcanchecauich@gmail.com>
2021-01-16 17:02:32 -0600

    modelos base - falta clinica

--- src/main/java/com/example/demo/model/Medico.java
+++ src/main/java/com/example/demo/model/Medico.java
@@ -14,72 +14,49 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "doctor")
 public class Medico {
 
     @Id
-    @Getter
-    @Setter
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="doctor_id")
     private Integer doctor_id;
 
-    @Getter
-    @Setter
     @Column(name="clinica_id")
     private Integer clinica_id;
 
-    @Getter
-    @Setter 
     @Column(name="nombre")
     private String nombre;
     
-    @Getter
-    @Setter
     @Column(name="apellido_paterno")
     private String apellidoPaterno;
 
-    @Getter
-    @Setter
     @Column(name="apellido_materno")
     private String apellidoMaterno;
 
-    @Getter
-    @Setter
     @Column(name="curp")
     private String curp;
     
-    @Getter
-    @Setter
     @Column(name="urlCedulta")
     private String urlCedulta;
 
-    @Getter
-    @Setter
     @Column(name="url_foto")
     private String urlFoto;
 
-    @Getter
-    @Setter
     @Column(name="especialidad")
     private String especialidad;
 
-    @Getter
-    @Setter
     @Column(name="status")
     private String status;
 
-    @Getter
-    @Setter
     @Column(name="email")
     private String email;
 
-    @Getter
-    @Setter
     @Column(name="telefono")
     private String telefono;
 
-    @Getter
-    @Setter
     @Column(name="user_id")
     private String userId;
 
